05830c517bdcc314df5e602f6d2d006387fcc260
EdwinLai <edwin_lai@kingston.corp-partner.google.com>
2018-12-14 00:21:23 +0800

    playing travis.yml

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import sys
 
 if '__main__' == __name__:
-    kwargs = dict(x.split('=', 1)) for x in sys.argv[1:] if x.find('=') != -1)
+    kwargs = dict(x.split('=', 1) for x in sys.argv[1:] if x.find('=') != -1)
     args = list(x for x in sys.argv[1:] if x.find('=') == -1)
 
     print args
     print kwargs
 
-    exit 0;
+    exit(0);
