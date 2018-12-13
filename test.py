f0b49ca31f39dac55fb2884a09333e85ed03e749
EdwinLai <edwin_lai@kingston.corp-partner.google.com>
2018-12-14 00:08:07 +0800

    playing travis.yml

---
+++ test.py
@@ -0,0 +1,10 @@
+import sys
+
+if '__main__' == __name__:
+    kwargs = dict(x.split('=', 1)) for x in sys.argv[1:] if x.find('=') != -1)
+    args = list(x for x in sys.argv[1:] if x.find('=') == -1)
+
+    print args
+    print kwargs
+
+    exit 0;
